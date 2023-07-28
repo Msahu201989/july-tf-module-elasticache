5f9d4a34d2a75f06b934c9a47493593ce1a99e59
Mukesh <Stunty@007>
2023-07-28 12:30:03 +0530

    July 23 learning devops

---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "env" {}
+variable "name" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "subnets" {}
+variable "vpc_id" {}
+variable "vpc_cidr" {}
+
